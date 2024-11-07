d05cc56c7c8e5e89fe0495f6338a11b5556939ce
19526350Pa <128559657+19526350Pa@users.noreply.github.com>
2024-11-06 21:36:57 -0300

    d

---
+++ informacoesGlobais.js
@@ -0,0 +1,8 @@
+onst url="https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/transporte/transporte-dados-globais.json"
+
+async function visualizarInformaçõesGlobais(){
+    constant resposta = await fetch (url)
+    let dados = await resposta.json;
+    console.log( dados );
+}
+visualizarInformaçõesGlobais();
